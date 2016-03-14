f3578d4f7810d36764ffd9a44fc47784b8049bc1
Chuev, Aleksandr <a.chuev@itransition.com>
2016-03-14 17:44:23 +0300

    Implemented files uploading

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var open = require('open');
+var multer = require('multer');
 
 var app = express();
 
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'images');
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + '.jpg');
+	}
+});
+var upload = multer({storage: storage});
+
 app.use(express.static('app'));
 app.use(express.static('node_modules'));
+
+app.post('/upload', upload.single('image'), function (req, res) {
+	res.sendStatus(200);
+});
 
 app.listen(80, function () {
 	open('http://localhost/');
