1ca03dbd645be0fffce0c02d05e37aa7c0956af5
Roman, Lukashik <r.lukashik@itransition.com>
2016-03-15 22:30:56 +0300

    Added average method into the number helper

--- app/helpers/__tests__/numberHelper.js
+++ app/helpers/__tests__/numberHelper.js
@@ -8,4 +8,12 @@ describe('numberHelper', function () {
 			expect(numberHelper.abs(2)).toEqual(2);
 		}));
 	});
+
+	describe('average method', function() {
+		it('works correctly', inject(function(numberHelper) {
+			expect(numberHelper.average(1, 2, 3)).toEqual(2);
+			expect(numberHelper.average(1, 0, 5)).toEqual(2);
+			expect(numberHelper.average(0, 0, 0)).toEqual(0);
+		}))
+	})
 });
--- app/helpers/numberHelper.js
+++ app/helpers/numberHelper.js
@@ -8,6 +8,18 @@ angular.module('app').factory('numberHelper', [function () {
 		 */
 		abs: function (value) {
 			return (value ^ (value >> 31)) - (value >> 31);
+		},
+
+		/**
+		 * Returns the average value of the color
+		 *
+		 * @param {number} red
+		 * @param {number} green
+		 * @param {number} blue
+		 * @returns {number}
+		 */
+		average: function(red, green, blue) {
+			return (red + green + blue) / 3;
 		}
 	};
 }]);
