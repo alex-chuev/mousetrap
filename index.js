d6d0832a1b6828bcfd67f563d6e7b626863dd530
Roman, Lukashik <r.lukashik@itransition.com>
2016-03-15 22:29:18 +0300

    Changed the approach of saving an image

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var express = require('express');
 var open = require('open');
-var multer = require('multer');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -11,22 +11,19 @@ var IMAGE_FOLDER = 'images';
 var IMAGE_EXTENSION = '.jpg';
 var SERVER_PORT = 8080;
 
-var storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, IMAGE_FOLDER);
-	},
-	filename: function (req, file, cb) {
-		cb(null, Date.now() + IMAGE_EXTENSION);
-	}
-});
-var upload = multer({storage: storage});
-
+app.use(bodyParser.json());
 app.use(express.static('app'));
 app.use(express.static('node_modules'));
 app.use('/' + IMAGE_FOLDER, express.static(IMAGE_FOLDER));
 
-app.post('/upload', upload.single('image'), function (req, res) {
-	res.sendStatus(200);
+app.post('/' + IMAGE_FOLDER, function (req, res) {
+	var imagePath = IMAGE_FOLDER + '/' + Date.now() + IMAGE_EXTENSION,
+		imageBase64 = req.body.data.replace(/^data:image\/jpeg;base64,/, ''),
+		image = new Buffer(imageBase64, 'base64');
+
+	fs.writeFile(imagePath, image, 'base64', function(err) {
+		res.sendStatus(err ? 500 : 200);
+	});
 });
 
 app.get('/' + IMAGE_FOLDER, function (req, res) {
